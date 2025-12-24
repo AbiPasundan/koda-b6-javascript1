120a99d7a0ae3e45113af1f9592fa93c70a10c12
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2025-12-24 10:47:21 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,14 @@
+const celcius = 40
+
+
+const celciusToFarenheit = (celcius * 9/5) + 32
+const celciusToReamure = celcius * 4 / 5
+const celciusToKelvin = celcius + 273
+
+// c to f
+console.log(`Hasil konversi suhu dari celcius ke Farenheit adalah ${celciusToFarenheit}`);
+
+// c to r
+console.log(`Hasil konversi suhu dari celcius ke Reamure adalah ${celciusToReamure}`);
+
+console.log(`Hasil konversi suhu dari celcius ke Kelvin adalah ${celciusToKelvin}`);
