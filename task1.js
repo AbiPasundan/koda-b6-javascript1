60018bc74c36bbd8204d7cb54c01e41d2f078fcf
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2025-12-24 16:25:31 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -10,14 +10,43 @@
 // console.log(`Hasil konversi suhu dari celcius ke Kelvin adalah ${celciusToKelvin}`);
 
 
+
+// 2
+// process.stdout.write('Enter a number: ');
+// process.stdin.on('data', (data) => {
+//     // deklarasi
+//     const r = data
+//     const phi = 3.14
+//     // perhitungan
+//     const l = phi * r * r
+//     const d = 2 * r
+//     const k = 2 * phi * r
+//     console.log(`Luasnya dari lingkaran adalah : ${l}`);
+//     console.log(`Diameter dari lingkaran adalah : ${d}`);
+//     console.log(`Keliling dari lingkaran adalah : ${k}`);
+//     process.exit();
+// });
+
 //v1
-let r = 4
-const phi = 3.14
+let r = 7
+let phi = 3.14
 
-const l = phi * r * r 
-const d = 2 * r 
-const k = 2 * phi * r 
+if(phi === 3.14) {
+    const l = phi * r * r 
+    const d = 2 * r 
+    const k = 2 * phi * r 
 
-console.log("jari-jari lingkaran adalah, "+ l);
-console.log("diameter lingkaran adalah, "+ d);
-console.log("keliling lingkaran adalah, "+ k);
+    console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
+    console.log(`luas diameter dengan r nya ${r} adalah ${ d}`);
+    console.log(`luas keliling dengan r nya ${r} adalah ${ k}`);
+}else if(phi === 22/7){
+    const l = phi * r * r 
+    const d = 2 * r 
+    const k = 2 * phi * r 
+
+    console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
+    console.log(`luas diameter dengan r nya ${r} adalah ${ d}`);
+    console.log(`luas keliling dengan r nya ${r} adalah ${ k}`);
+} else {
+    console.log("masukan phi yang benar");
+}
