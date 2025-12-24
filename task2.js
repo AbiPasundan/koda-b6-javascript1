ac8135529f8bd0d4721f0156243decefcb09d6cd
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2025-12-24 14:12:06 +0700

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,60 @@
+const we = {
+    are: {
+        the: {
+            best: "koda"
+        }
+    }
+}
+
+console.log(we.are.the.best)
+
+const hello = {
+    world: "Hello World" 
+}
+
+console.log(hello.world)
+
+const obj = {
+    str: [0,1,2,[0,[0,1, {
+        man: [
+            {
+                tech: {
+                    academy: "Tech Academy"
+                }
+            }
+        ]
+    }]]]
+}
+
+console.log(obj.str[3][1][2].man[0].tech.academy)
+
+
+const my = [{
+    favourite: [0,1,2, {
+        fruit: {
+            is: "Apple"
+        }
+    }]
+}]
+
+console.log(my[0].favourite[3].fruit.is)
+
+
+
+
+
+
+
+
+
+
+
+
+const num = {
+    fisrt: [0, 30],
+
+    second: [0,1,2]
+}
+
+
+console.log(num.fisrt[1] + num.second[2])
