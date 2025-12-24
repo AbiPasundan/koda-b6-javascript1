a2fb50708f6ccdc5445f36288bff6e2dddfd7dcb
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2025-12-24 20:24:04 +0700

    Improvement code. supaya bisa menginputkan angka

--- task1.js
+++ task1.js
@@ -28,47 +28,59 @@
 // });
 
 //v1
-let r = 7
-let phi = 22/7
-
-// if(phi === 3.14) {
-//     const l = phi * r * r 
-//     const d = 2 * r 
-//     const k = 2 * phi * r 
-
-//     console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
-//     console.log(`luas diameter dengan r nya ${r} adalah ${ d}`);
-//     console.log(`luas keliling dengan r nya ${r} adalah ${ k}`);
-// }else if(phi === 22/7){
-//     const l = phi * r * r 
-//     const d = 2 * r 
-//     const k = 2 * phi * r 
-
-//     console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
-//     console.log(`luas diameter dengan r nya ${r} adalah ${ d}`);
-//     console.log(`luas keliling dengan r nya ${r} adalah ${ k}`);
+// let r = 7
+// let phi = 22/7
+// if(phi === 3.14 || phi === 22/7){
+//     if(phi === 3.14){
+//         const l = phi * r * r 
+//         const d = 2 * r 
+//         const k = 2 * phi * r 
+//         console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
+//         console.log(`luas diameter dengan r nya ${r} adalah ${ d}`);
+//         console.log(`luas keliling dengan r nya ${r} adalah ${ k}`);
+//     } else if (phi === 22/7){
+//         const l = phi * r * r 
+//         const d = 2 * r 
+//         const k = 2 * phi * r 
+//         console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
+//         console.log(`luas diameter dengan r nya ${r} adalah ${ d}`);
+//         console.log(`luas keliling dengan r nya ${r} adalah ${ k}`);
+//     }
 // } else {
-//     console.log("masukan phi yang benar");
+//     console.log("stop");
 // }
 
-if(phi === 3.14 || phi === 22/7){
-    if(phi === 3.14){
-        const l = phi * r * r 
-        const d = 2 * r 
-        const k = 2 * phi * r 
 
-        console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
-        console.log(`luas diameter dengan r nya ${r} adalah ${ d}`);
-        console.log(`luas keliling dengan r nya ${r} adalah ${ k}`);
-    } else if (phi === 22/7){
-        const l = phi * r * r 
-        const d = 2 * r 
-        const k = 2 * phi * r 
 
-        console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
-        console.log(`luas diameter dengan r nya ${r} adalah ${ d}`);
-        console.log(`luas keliling dengan r nya ${r} adalah ${ k}`);
-    }
-} else {
-    console.log("stop");
+// v2
+process.stdout.write('Masukan nilai jari-jari: ');
+process.stdin.on('data', (data) => {
+    const r = Number(data.toString().trim())
+
+    if(!Number.isFinite(r)){
+        console.log("yang anda masukan bukan angka!")
+        process.exit()
+    } else {
+        let phi = 3.14
+        if(phi === 3.14) {
+            const l = phi * r * r 
+            const d = 2 * r 
+            const k = 2 * phi * r 
+
+            console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
+            console.log(`diameter lingkaran dengan r nya ${r} adalah ${ d}`);
+            console.log(`keliling lingkaran dengan r nya ${r} adalah ${ k}`);
+        }else if(phi === 22/7){
+            const l = phi * r * r 
+            const d = 2 * r 
+            const k = 2 * phi * r 
+
+            console.log(`luas lingkaran dengan r nya ${r} adalah ${ l}`);
+            console.log(`diameter lingkaran dengan r nya ${r} adalah ${ d}`);
+            console.log(`keliling lingkaran dengan r nya ${r} adalah ${ k}`);
+        } else {
+            console.log(`Tentukan phi yang benar`);
+        }
+        process.exit();
 }
+});
